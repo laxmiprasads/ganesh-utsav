425ea635a0e5a78067879bbd93d2deb2970fbe40
Laxmi Prasad <s.laxmiprasad2006@gmail.com>
2026-09-25 16:20:40 +0530

    Configure production API URL

--- ganesh-utsav-frontend/src/app/core/services/api.service.ts
+++ ganesh-utsav-frontend/src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Auction, Category, Contribution, DashboardStats, Expense, PublicContribution } from '../models/api-models';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
---
+++ ganesh-utsav-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ganesh-utsav-production.up.railway.app/api'
+};
